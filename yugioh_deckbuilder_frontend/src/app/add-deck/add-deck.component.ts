0e988112b179fe62b660b743147a34687bce45d9
Diana <dianas@outlook.at>
2023-02-26 15:56:58 +0100

    started working on drag and drop responses

--- yugioh_deckbuilder_frontend/src/app/add-deck/add-deck.component.ts
+++ yugioh_deckbuilder_frontend/src/app/add-deck/add-deck.component.ts
@@ -1,4 +1,4 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,13 +10,31 @@ export class AddDeckComponent {
 
   mainDeck = ['Blue-Eyes White Dragon', 'Rabidragon', 'Phantasm Spiral Dragon'];
 
+  sideDeck = ['Chinese Chomping Cabbage'];
+
+
     cards = ['Spiral Serpent', 'Tri-Horned Dragon', 'Sengenjin', 'Your Mom', 'Wing Weaver'];
 
-    drop(event: CdkDragDrop<string[]>) {
+    move(event: CdkDragDrop<string[]>) {
       if (event.previousContainer === event.container) {
         moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
       } else {
-        transferArrayItem(
+          copyArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex,
+        );
+      }
+    }
+
+    remove(event: CdkDragDrop<string[]>){
+      if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      }
+
+      else{
+          transferArrayItem(
           event.previousContainer.data,
           event.container.data,
           event.previousIndex,
